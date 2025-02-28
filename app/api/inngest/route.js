ceed1f450fcff2c88188c3d28a05525cc7875790
adiaparmar <adiaparmar@gmail.com>
2025-02-28 20:03:28 +0530

    Add generateIndustryInsights function and integrate with Inngest API

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,8 @@
 import { inngest } from "@/lib/inngest/client";
-import {serve} from "inngest/next";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
+import { serve } from "inngest/next";
 
-
-export const {GET, POST, PUT} = serve({
+export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-
-  ]
-})
+  functions: [generateIndustryInsights],
+});
