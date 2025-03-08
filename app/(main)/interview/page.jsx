d1fa8f5d9681d850c1776483af2d3691119997ee
adiaparmar <adiaparmar@gmail.com>
2025-03-08 20:44:35 +0530

    Add assessment retrieval and performance dashboard components

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { getAssessments } from "@/actions/interview";
+import StatsCards from "./_components/stats-cards";
+import PerformanceChart from "./_components/performance-chart";
+import QuizList from "./_components/quiz-list";
 
-function page() {
-  return <div>Interview</div>;
-}
+const InterviewPage = async () => {
+  const assessments = await getAssessments();
+  return (
+    <div>
+      <div>
+        <h1 className="text-6xl font-bold gradient-title mb-5">
+          Interview Preparation
+        </h1>
 
-export default page;
+        <div className="space-y-6">
+          <StatsCards assessments={assessments} />
+          <PerformanceChart assessments={assessments} />
+          <QuizList assessments={assessments} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterviewPage;
